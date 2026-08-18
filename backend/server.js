7f76cfcba270f9a4ca9ad0cda2185ad6126fc21e
Davi-moura <daviarm2008@gmail.com>
2026-08-18 09:11:44 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -189,6 +189,6 @@ app.get("/medicacoes", (req, res) => {
 // START
 const PORT = process.env.PORT
        || 3000;
-app.listener(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Porta ${PORT}`);
 });
